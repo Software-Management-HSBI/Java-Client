8cea3cb82f2302621621542a1232d729e976814b
Bent Schöne <bent.schoene@fh-bielefeld.de>
2023-12-01 14:51:55 +0100

    add javadoc for Player.java and Util.java

--- app/src/main/java/Player.java
+++ app/src/main/java/Player.java
@@ -13,6 +13,12 @@ public class Player {
     public Texture left;
     public int x, y;
 
+    /**
+     * Create a new player on the given x and y coordinates with set textures
+     *
+     * @param x position of the player
+     * @param y position of the player
+     */
     public Player(int x, int y) {
         this.texture = LoadTexture(Constants.SPRITETEXTUREPATH + "player_straight.png");
         this.straight = scale(LoadTexture(Constants.SPRITETEXTUREPATH + "player_straight.png"));
@@ -22,18 +28,27 @@ public class Player {
         this.y = y;
     }
 
+    /** Set the texture of the player to the straight texture */
     public void driveStraight() {
         this.texture = straight;
     }
 
+    /** Set the texture of the player to the right texture */
     public void driveRight() {
         this.texture = right;
     }
 
+    /** Set the texture of the player to the left texture */
     public void driveLeft() {
         this.texture = left;
     }
 
+    /**
+     * Scale the given texture by a factor of 3
+     *
+     * @param texture to scale
+     * @return the scaled texture
+     */
     public Texture scale(Texture texture) {
         Image image = LoadImageFromTexture(texture);
         ImageResize(image, this.texture.width() * 3, this.texture.height() * 3);
