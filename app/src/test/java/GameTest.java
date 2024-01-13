9dc25166a88307c6902681cb38867ee99f23a3a5
MatteoXAntoz <108668752+MatteoXAntoz@users.noreply.github.com>
2024-01-13 06:03:35 +0100

    Create GameTest.java

---
+++ app/src/test/java/GameTest.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+
+class GameTest {
+
+    @Test
+    void gameLoop() {}
+
+    @Test
+    void resetRoad() {}
+
+    @Test
+    void update() {}
+
+    @Test
+    void render() {}
+
+    @Test
+    void createPlayer() {}
+
+    @Test
+    void createBackground() {}
+
+    @Test
+    void drawTextureParallax() {}
+
+    @Test
+    void renderSegmentSprites() {}
+
+    @Test
+    void addSprite() {}
+}
